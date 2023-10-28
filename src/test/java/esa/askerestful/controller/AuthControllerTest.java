b38cf1c6fb6461119b968f9f61a3010fa4537e4c
esa <takutoakbar354@gmail.com>
2023-10-29 00:03:53 +0700

    coba

--- src/test/java/esa/askerestful/controller/AuthControllerTest.java
+++ src/test/java/esa/askerestful/controller/AuthControllerTest.java
@@ -9,13 +9,13 @@ import esa.askerestful.model.WebResponse;
 import esa.askerestful.repository.UserRepository;
 import esa.askerestful.security.BCrypt;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -33,9 +33,13 @@ class AuthControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @BeforeEach
+    void setUp(){
+        userRepository.deleteAll();
+    }
+
     @Test
     void testLoginBadRequest()throws Exception{
-
         LoginUserRequest request = new LoginUserRequest();
         request.setUsername("user salah");
         request.setPassword("salah");
@@ -51,6 +55,68 @@ class AuthControllerTest {
             WebResponse<TokenResponse> response = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<>() {
             });
             Assertions.assertNotNull( response.getErrors());
+        });
+    }
+
+    @Test
+    void loginFailedWrongPassword()throws Exception{
+        User user = new User();
+        user.setIdUser("User_123");
+        user.setUsername("esa");
+        user.setEmail("esa@example.com");
+        user.setPassword(BCrypt.hashpw("esa123" , BCrypt.gensalt()));
+        userRepository.save(user);
+
+        LoginUserRequest request = new LoginUserRequest();
+        request.setUsername("esa");
+        request.setPassword("esa112");
+
+        mockMvc.perform(
+                post("/api/auth/user")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request))
+        ).andExpectAll(
+                status().isUnauthorized()
+        ).andDo(result -> {
+            WebResponse<String> response = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<>() {
+            });
+            Assertions.assertNotNull(response.getErrors());
+        });
+    }
+
+    @Test
+    void loginSuccess()throws Exception{
+        User user = new User();
+        user.setIdUser("User_123");
+        user.setUsername("esa");
+        user.setEmail("esa@example.com");
+        user.setPassword(BCrypt.hashpw("esa123" , BCrypt.gensalt()));
+        userRepository.save(user);
+
+        LoginUserRequest request = new LoginUserRequest();
+        request.setUsername("esa");
+        request.setPassword("esa123");
+
+        mockMvc.perform(
+                post("/api/auth/user")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request))
+        ).andExpectAll(
+                status().isOk()
+        ).andDo(result -> {
+            WebResponse<TokenResponse> response = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<>() {
+            });
+            assertNull(response.getErrors());
+            assertNotNull(response.getData().getToken());
+            assertNotNull(response.getData().getExpiredAt());
+
+            User userDb = userRepository.findById(user.getIdUser()).orElse(null);
+            assertNotNull(userDb);
+            assertEquals(userDb.getToken() , response.getData().getToken());
+            assertEquals(userDb.getTokenExpiredAt() , response.getData().getExpiredAt());
+
         });
     }
 }
